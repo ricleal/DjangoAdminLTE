ac4d0761f673229ddccce3c0c85c3fae4c563205
Ricardo Ferraz Leal <ricleal@gmail.com>
2015-12-30 23:21:38 -0500

    example for configuration view. TODO: table for instrument

--- server/configuration/views.py
+++ server/configuration/views.py
@@ -1,11 +1,29 @@
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, FormView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
 
-import importlib
-from .models import BioSANSCommon
+import logging
+
+logger = logging.getLogger('configuration')
+
+def import_string(import_name):
+    '''
+    Import class from String
+    '''
+    try:
+        if '.' in import_name:
+            module, obj = import_name.rsplit('.', 1)
+            return getattr(__import__(module, None, None, [obj]), obj)
+        else:
+            return __import__(import_name)
+    except (ImportError, AttributeError), e:
+        print e
 
 # Create your views here.
-class ConfigurationList(ListView):
-    model = importlib.import_module(settings.INSTRUMENT_MODULES["BIOSANS"]["model_common"])
-
+#@login_required
+class ConfigurationList(LoginRequiredMixin,ListView):
+    template_name = 'configuration/configuration_list.html'
+    # def get(self, request, *args, **kwargs):
+    #     instrument = request.session['instrument']
+    #     self.model = import_string(settings.INSTRUMENT_MODULES[instrument]["model_common"])
+    model = import_string(settings.INSTRUMENT_MODULES['BIOSANS']["model_common"])
