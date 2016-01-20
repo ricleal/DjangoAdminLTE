24fa476b17d0e78d7dd76c0bd89c1e0ad2388e2b
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-01-20 13:45:16 -0500

    Populate spreadsheet from the catalog is working

--- server/sans/eqsans/views.py
+++ server/sans/eqsans/views.py
@@ -21,7 +21,7 @@ class ConfigurationList(LoginRequiredMixin, ListView):
     '''
     template_name = 'sans/eq-sans/configuration_list.html'
     #model = EQSANSConfiguration
-    
+
     def get_queryset(self):
         ConfigurationList.queryset = EQSANSConfiguration.objects.filter(user = self.request.user)
         return ListView.get_queryset(self)
@@ -32,14 +32,14 @@ class ConfigurationDetail(LoginRequiredMixin, DetailView):
     '''
     template_name = 'sans/eq-sans/configuration_detail.html'
     model = EQSANSConfiguration
-    
+
 #     def render_to_response(self, context, **response_kwargs):
 #         '''
 #         Just to log the context
 #         '''
 #         logger.debug(pformat(context))
 #         return super(ConfigurationDetail, self).render_to_response(context, **response_kwargs)
-        
+
 
 class ConfigurationCreate(LoginRequiredMixin, CreateView):
     '''
@@ -47,7 +47,7 @@ class ConfigurationCreate(LoginRequiredMixin, CreateView):
     '''
     template_name = 'sans/eq-sans/configuration_form.html'
     form_class = ConfigurationForm
-    
+
 #     def get_initial(self):
 #         # Get the initial dictionary from the superclass method
 #         initial = super(ConfigurationCreate, self).get_initial()
@@ -56,7 +56,7 @@ class ConfigurationCreate(LoginRequiredMixin, CreateView):
 #         initial['user'] = self.request.user.pk
 #         initial['instrument'] = get_object_or_404(Instrument, name=instrument_name)
 #         return initial
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.instrument = get_object_or_404(Instrument, name=instrument_name)
@@ -74,7 +74,7 @@ class ConfigurationUpdate(LoginRequiredMixin, UpdateView):
 #######################################################################
 #
 # Reduction
-# 
+#
 #######################################################################
 
 class ReductionList(LoginRequiredMixin, ListView):
@@ -89,19 +89,20 @@ class ReductionList(LoginRequiredMixin, ListView):
 
 
 class EntryMixin(object):
-    
+
     def get_context_data(self, **kwargs):
         logger.debug(pformat(kwargs))
         logger.debug(pformat(self.kwargs))
-        context = super(EntryMixin, self).get_context_data(**kwargs)        
+        context = super(EntryMixin, self).get_context_data(**kwargs)
         context["entry_headers"] = EQSANSEntry.get_field_titled_names()
         context["entry_names"] = EQSANSEntry.get_field_names()
         return context
-    
+
     def form_valid(self, form):
         '''
         Stores the handsontable in a variable
         '''
+        logger.debug(self.request.POST["entries_hidden"]);
         self.handsontable = json.loads(self.request.POST["entries_hidden"])
         return super(EntryMixin, self).form_valid(form)
 
@@ -109,15 +110,15 @@ class EntryMixin(object):
 class ReductionDetail(LoginRequiredMixin, DetailView):
     '''
     Detail of a Reduction
-    
+
     A Reduction is a title and a set of entries.
     The entries are an hidden field : id="entries_hidden"
     Which are an Handsontable
-    
+
     '''
     template_name = 'sans/eq-sans/reduction_detail.html'
     model = EQSANSReduction
-    
+
     #TODO: Get query set by user
 
 
@@ -129,15 +130,15 @@ class ReductionCreate(LoginRequiredMixin,EntryMixin, CreateView):
     model = EQSANSReduction
     fields = '__all__'
     handsontable = None
-    
+
     def get_success_url(self):
         '''
         Called after the reduction was saved on the DB (after form_valid)
         It creates The entries for this reduction
         '''
-        EQSANSEntry.objects.create_entries_from_handsontable(self.handsontable, reduction=self.object)            
+        EQSANSEntry.objects.create_entries_from_handsontable(self.handsontable, reduction=self.object)
         return super(ReductionCreate, self).get_success_url()
-    
+
 class ReductionUpdate(LoginRequiredMixin,EntryMixin, UpdateView):
     '''
     Detail of a Reduction
@@ -145,24 +146,24 @@ class ReductionUpdate(LoginRequiredMixin,EntryMixin, UpdateView):
     template_name = 'sans/eq-sans/reduction_form.html'
     model = EQSANSReduction
     fields = '__all__'
-    
+
     def get_context_data(self, **kwargs):
-        '''        
+        '''
         Get all entries for this reduction and add them as json to the context
-        This will poplulate the table 
+        This will poplulate the table
         '''
         context = super(ReductionUpdate, self).get_context_data(**kwargs)
         entries = EQSANSEntry.objects.filter(reduction__configuration__user = self.request.user,
                                                         reduction__id = self.kwargs['pk']).values()
-        entries_json = json.dumps(list(entries), cls=DjangoJSONEncoder)                                              
+        entries_json = json.dumps(list(entries), cls=DjangoJSONEncoder)
         context["entries"] = entries_json
         return context
-    
+
     def get_success_url(self):
         '''
         Called after the reduction was saved on the DB (after form_valid)
         It deletes all Entries first and then create new ones with the table
         '''
         EQSANSEntry.objects.filter(reduction = self.object).delete()
-        EQSANSEntry.objects.create_entries_from_handsontable(self.handsontable, reduction=self.object)            
+        EQSANSEntry.objects.create_entries_from_handsontable(self.handsontable, reduction=self.object)
         return super(ReductionUpdate, self).get_success_url()
