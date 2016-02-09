097849d056a35e3ab5df1ef13ce47f8cbe621b1a
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2016-02-09 07:58:15 -0500

    New models for Jobs

--- server/jobs/models.py
+++ server/jobs/models.py
@@ -7,25 +7,18 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 from server.catalog.models import Instrument
+from ipyparallel.client.remotefunction import remote
 
-class Job(models.Model):
+
+class Transaction(models.Model):
     '''
-    Job will have a foreign key to here
-    The same configuration can launch multiple jobs!
+    A transaction has jobs
+    Multiple jobs run in the same transaction folder
     '''
     
-    MY_STATUS = ((0,'NOT_SUBMITTED'), (1, 'SUBMITTED'),)
-    FERMI_STATUS = ((3,'RUNNING'),
-                    (4,'QUEUED'), 
-                    (5,'COMPLETED'), 
-                    (6,'REMOVED'), 
-                    (7,'DEFERRED'), 
-                    (8,'IDLE'), 
-                    (9,'UNKNOWN'),)
-    
-    status = models.IntegerField(choices =  MY_STATUS + FERMI_STATUS, default = MY_STATUS[0])
-    
-    script = models.TextField(max_length=10240)
+    # Those are returned when a transaction is created
+    remote_id = models.IntegerField()
+    remote_directory = models.CharField(max_length=256)
     
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
@@ -38,11 +31,46 @@ class Job(models.Model):
                              related_name="job_users",
                              related_query_name="job_user",)
     
+    
+class Job(models.Model):
+    '''
+    Job will have a foreign key to here
+    The same configuration can launch multiple jobs!
+    '''
+    
+    LOCAL_STATUS = ((0,'NOT_SUBMITTED'), (1, 'SUBMITTED'),)
+    
+    REMOTE_STATUS = ((3,'RUNNING'),
+                    (4,'QUEUED'), 
+                    (5,'COMPLETED'), 
+                    (6,'REMOVED'), 
+                    (7,'DEFERRED'), 
+                    (8,'IDLE'), 
+                    (9,'UNKNOWN'),)
+    
+    local_status = models.IntegerField(choices =  LOCAL_STATUS, default = LOCAL_STATUS[0])
+    remote_status = models.IntegerField(choices =  REMOTE_STATUS, blank=True)
+    
+    script = models.TextField(max_length=10240)
+    
+    # Those represent the remote job: "CompletionDate", "StartDate", "SubmitDate":
+    remote_submit_date = models.DateTimeField()
+    remote_start_date = models.DateTimeField()
+    remote_complete_date = models.DateTimeField()
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    
     # Generic Relation for a reduction
     # Table Job will have 2 fields which are foreign keys to other tales: table_name, table.pk
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    
+    user = models.ForeignKey(Transaction, on_delete=models.CASCADE,
+                             related_name="transactions",
+                             related_query_name="transaction",)
+        
     
     class Meta:
         ordering = ["id"]
